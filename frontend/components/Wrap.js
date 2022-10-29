a63cea458186ac91a5f95fadcfe9e0b732739028
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2022-10-29 17:43:10 +0200

    Table finished

--- frontend/components/Wrap.js
+++ frontend/components/Wrap.js
@@ -4,38 +4,53 @@ import Select from './Select';
 
 
 const data = [
-  { action: "Long", coin: "ETH", leverage: "2x", networth: 20 },
-  { action: "Short", coin: "BTC", leverage: "1.5x", networth: 5 },
+  { id: 0, action: "Long", coin: "ETH", leverage: "2x", networth: 20 },
+  { id: 1, action: "Short", coin: "BTC", leverage: "1.5x", networth: 5 },
+  { id: 2, action: "Long", coin: "SHIBA", leverage: "2.5x", networth: 3 },
+  { id: 3, action: "Short", coin: "DOGE", leverage: "3x", networth: 100 },
 ]
 
 function Wrap() {
-  const [type, setType] = useState(data[0]); // Position type
   const [amount, setAmount] = useState(0); // Amount
-  const [showForm, setShowForm] = React.useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [key, setKey] = useState(-1);
 
   const Form = () => (
     <Card>
-      <h1 className='text-2xl font-bold mb-4'>Trade</h1>
+      <h1 className='text-2xl font-bold mb-4'>Trade X token for {data[key].coin}</h1>
       <form onSubmit={handleWrap} className='flex gap-4 flex-col'>
+        <p className='description'></p>
         {/* Enter amount */}
-        <input min={0} value={amount} onChange={(e) => setAmount(e.target.value)} type="number" placeholder={`${type.from} amount`} />
-        {/* Open position */}
+        <input min={0} value={amount} onChange={(e) => setAmount(e.target.value)} type="number" placeholder="Amount" />
+        {/* Open position */} 
         <button className='bg-violet-500 hover:bg-violet-400  active:bg-violet-600 shadow-xl'>
           {/* Here check if the values are entered */}
           Open position
         </button> 
-      </form>
+      </form> 
     </Card>
   )
 
   const handleWrap = (e) => {
-    console.log("Form submitted!", e)
+    var pos = data[key] // Get to data through index using key
+    alert(`Form submitted! ${pos.id}`) // Failed: Display the data using key
   }
 
   const handleTableEnter = (key, e) => {
-    console.log("Position chosen", key, e)
     setShowForm(true);
+    setKey(key);
   }
+
+  var descElement = this.getElementsByClassName("description");
+  descElement.innerHTML="Enter amount of coins you want to long";
+  // if(data[key].action == "Long"){  // <= you can put your condition here
+  //   descElement.innerHTML="Enter amount of coins you want to long";
+  // }else if (data[key].action == "Short"){
+  //   descElement.innerHTML="Enter amount in USDC to be used as collateral for short"; 
+  // }
+  // else {
+  //   descElement.innerHTML="Error";
+  // }
 
   return (
     <div>
@@ -51,7 +66,8 @@ function Wrap() {
         {data.map((val, key) => {
           return (
             <tr key={key}>
-              <td><button onClick={handleTableEnter(key)}>Enter</button></td>
+              {/* handleTableEnter is triggered all the time */}
+              <td><button onClick={() => handleTableEnter(key)}>Enter</button></td> 
               <td>{val.action}</td>
               <td>{val.coin}</td>
               <td>{val.leverage}</td>
